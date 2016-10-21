6f964d8c9860f7001f2ebe67675eae9fceab1294
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2016-10-21 15:34:48 -0300

    nuevos controles

--- js/main.js
+++ js/main.js
@@ -7,14 +7,16 @@ var GameState = {
 		this.load.image('brick', 'assets/brick_1.png');
 		this.load.image('bg', 'assets/bg_cyan.png');
 		this.load.image('char', 'assets/char.png');
-		this.load.image('arrow', 'assets/arrow.png');
+		this.load.image('arrow', 'assets/btn_arrow.png');
+		this.load.image('btn_up', 'assets/btn_up.png');
+		this.load.image('btn_empty', 'assets/btn_empty.png');
 		this.game.load.atlasJSONHash('char_turn', 'assets/char_turn.png', 'assets/char_turn.json');
     	this.game.load.audio('bgmusic', ['assets/audio/bgmusic.ogg']);
 	},
 	create:function () {
 		game.world.setBounds(0, 0, 640, 360);
 		music = this.game.add.audio('bgmusic');
-    	music.play();
+    	//music.play();
 
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.pageAlignHorizontally = true;
@@ -22,7 +24,7 @@ var GameState = {
     	this.game.stage.backgroundColor = '#18283C';
 
 		// var level = [1,1,1,1,2,-1,2,3];
-		var level_1 =[[1,2,1,1,2], [1,3,6,3,1], [3,2,7,4,2], [4,1,4,1,5] ,[4,2,3,4,3]];
+		var level_1 =[[1,2,6,1,2], [1,3,6,3,1], [3,2,7,4,2], [4,1,4,1,5] ,[4,2,3,4,3]];
 		// for (var i = level.length - 1; i >= 0; i--) {
 		// 	var distanceX = 60;
 		// 	var distanceY = 138;
@@ -45,12 +47,10 @@ var GameState = {
 
 				var brickTween = game.add.tween(brick.position.y);
 				brickTween.to({y:200}, 200, Phaser.Easing.Linear.None);
-				
 				brickTween.start();
 
-				//brick.tint = Math.random() * 0xffffff;
-				//brick.tint = (level_1[i][j]/10+Math.random()) * 0xffffff;
 				console.log(level_1[i][j]);
+				var bricks = [];
 			};
 			distanceY -=15;
 			stepY+=18;
@@ -58,31 +58,35 @@ var GameState = {
 			console.log(' ---- ')
 		};
 
+
+
 		viajero= game.add.sprite(this.game.world.centerX,this.game.world.centerY-30, 'char_turn');
 		viajero.anchor.setTo(0.5,.9);
 		viajero.customParams = {direction : 1};
-		viajero.animations.add('turn_1_2',	[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]);
+		viajero.animations.add('turn_1_2',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]);
 		viajero.animations.add('turn_2_3',[21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41]);
 		viajero.animations.add('turn_3_4',[41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61]);
 		viajero.animations.add('turn_4_1',[61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81]);
 
-		viajero.animations.add('turn_2_1',	[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21].reverse());
+		viajero.animations.add('turn_2_1',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21].reverse());
 		viajero.animations.add('turn_3_2',[21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41].reverse());
 		viajero.animations.add('turn_4_3',[41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61].reverse());
 		viajero.animations.add('turn_1_4',[61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81].reverse());
 
 		//left 
-		this.arrow_left = this.game.add.sprite(0,0,'arrow');
+		this.arrow_left = this.game.add.sprite(0,game.world.centerY,'arrow');
 		this.arrow_left.customParams = {direction :-1};
 		this.arrow_left.inputEnabled = true;
 		this.arrow_left.events.onInputDown.add(this.moveChar,this );
 
-		this.arrow_right = this.game.add.sprite(this.game.world.width,0,'arrow');
+		this.arrow_right = this.game.add.sprite(this.game.world.width,game.world.centerY,'arrow');
 		this.arrow_right.scale.x = -1;
 		this.arrow_right.customParams = {direction :1};
 		this.arrow_right.inputEnabled = true;
 		this.arrow_right.events.onInputDown.add(this.moveChar,this );
 
+		this.action_left = this.game.add.sprite(0,0,'btn_empty');
+		this.action_right = this.game.add.sprite(this.game.world.width-this.action_left.width,0,'btn_up');
 		
 	     linea = new Phaser.Line(this.game.world.centerX, 0, this.game.world.centerX, 360);
 
