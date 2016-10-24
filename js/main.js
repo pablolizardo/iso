6b8d3c22708632336fa26490feb81de4c07bf171
pablolizardo <pablo_lizardo@yahoo.com.ar>
2016-10-24 08:15:13 -0300

    k

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 var game = new Phaser.Game(640,360,Phaser.AUTO);
 var linea;
+var isoGroup, cursorPos, cursor;
 
+var ii = 0; 
+var jj = 0;
+var dx = 10;
+var dy = 10;
+var bdx = 21;
+var bdy = 21;
+
+var fps =60;
+var animTime = 400;
 var GameState = {
 	preload:function () {
 		console.log('preload');
-		// this.load.image('brick', 'assets/brick_1.png');
-		// this.load.image('bg', 'assets/bg_cyan.png');
-		// this.load.image('char', 'assets/char.png');
-		// this.load.image('arrow', 'assets/btn_arrow.png');
-		// this.load.image('btn_up', 'assets/btn_up.png');
-		// this.load.image('btn_empty', 'assets/btn_empty.png');
-		// this.game.load.atlasJSONHash('char_turn', 'assets/char_turn.png', 'assets/char_turn.json');
-		// this.game.load.atlasJSONHash('char_walk', 'assets/char_walk.png', 'assets/char_walk.json');
+		this.load.image('brick', 'assets/brick_1.png');
 		this.game.load.atlasJSONHash('viajero', 'assets/viajero.png', 'assets/viajero.json');
 		this.game.load.atlasJSONHash('ui', 'assets/ui.png', 'assets/ui.json');
+		this.game.load.atlasJSONHash('bricks', 'assets/bricks.png', 'assets/bricks.json');
     	this.game.load.audio('bgmusic', ['assets/audio/bgmusic.ogg']);
+
+    	this.centerX = this.game.world.centerX;
+    	this.centerY = this.game.world.centerY;
+    	this.w = this.game.world.width;
+    	this.h = this.game.world.height;
+
+	    game.plugins.add(new Phaser.Plugin.Isometric(game));
+	    game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
+	    game.iso.anchor.setTo(0.5, 0);
+
 	},
 	create:function () {
-		game.world.setBounds(0, 0, 640, 360);
+		//game.world.setBounds(0, 0, 640, 360);
 		music = this.game.add.audio('bgmusic');
-    	//music.play();
+    	music.play();
 
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.pageAlignHorizontally = true;
@@ -27,45 +41,50 @@ var GameState = {
     	this.game.stage.backgroundColor = '#18283C';
 
 		// var level = [1,1,1,1,2,-1,2,3];
-		var level_1 =[[1,2,6,1,2], [1,3,6,3,1], [3,2,7,4,2], [4,1,4,1,5] ,[4,2,3,4,3]];
-		// for (var i = level.length - 1; i >= 0; i--) {
-		// 	var distanceX = 60;
-		// 	var distanceY = 138;
-		// 	var brick = this.game.add.sprite(this.game.world.centerX+(distanceX/2)*i,200+(distanceX/2)-17*i,'brick');
-		// 	brick.anchor.setTo(0.5	);
-		// 	brick.position.y -= level[i]*(distanceX/2);
-		// };
-		// var distanceX = 62;
-		// var distanceY = 36;
-		// var stepY =0;
-		// var stepX =0;
+		// 
+		var level_1 =[];
+		for (var i = 0; i < dx; i++) {
+			for (var j = 0; j < dy; j++) {
+				level_1[[i],[j]]=i;
+				console.log(level_1[[i],[j]]);
+			}
+		}
+		//var level_height =[[1,2,6,1,2], [1,3,6,3,1], [3,2,7,4,2], [4,1,4,1,5] ,[4,2,3,4,3]];
+		//var level_matrix =[[1,1,0,0,1,1,1],[1,1,1,1,0], [1,0,1,1,1], [0,1,1,0,1], [1,0,1,1,1]];
+        
 
-		// for (var i = level_1.length - 1; i >= 0; i--) {
-		// 	for (var j = level_1[i].length - 1; j >= 0; j--) {
-		// 		var brick = this.game.add.sprite(this.game.world.centerX-150+j*31+stepX,this.game.world.centerY-18*j+stepY,'brick');
-		// 		// brick.anchor.setTo(0.5);
-		// 		brick.position.y -= level_1[i][j]*10;
-		// 		this.game.add.tween(brick).from( { y: -200 });
-		// 		this.game.add.tween(brick).start;
+        isoGroup = game.add.group();
 
-		// 		var brickTween = game.add.tween(brick.position.y);
-		// 		brickTween.to({y:200}, 200, Phaser.Easing.Linear.None);
-		// 		brickTween.start();
-
-		// 		console.log(level_1[i][j]);
-		// 		var bricks = [];
-		// 	};
-		// 	distanceY -=15;
-		// 	stepY+=18;
-		// 	stepX+=31;
-		// 	console.log(' ---- ')
-		// };
+		var cube; 
+		
+        for (var xx = bdx*dx; xx > 0; xx -= bdx) {
+            for (var yy = bdy*dy; yy > 0; yy -= bdy) {
+                cube = game.add.isoSprite(xx, yy, 0, 'brick', 0, isoGroup);
+                cube.anchor.set(0.5);
+                cube.scale.set(0.3);
+                cube.alpha =Math.random()/3;
+                // if (level_matrix[ii][jj] >0) {
+                // 	//cube.destroy();
+	               //  //game.add.tween(cube).to({ isoZ: 10 }, 100 * ((xx + yy) % 10), Phaser.Easing.Quadratic.InOut, true, 0, Infinity, true);
+                // }
+                game.iso.simpleSort(isoGroup);
+            	jj++;
+            }
+        	ii++;
+        }
 
 
+	
 
-		viajero= game.add.sprite(this.game.world.centerX,this.game.world.centerY, 'viajero');
+
+		viajero= game.add.sprite(this.game.world.centerX,this.game.world.centerY+5, 'viajero');
 		viajero.anchor.setTo(0.5,.9);
+		viajero.scale.set(0.5);
 		viajero.customParams = {direction : 1};
+
+    	game.camera.follow(viajero);
+
+
 		viajero.animations.add('turn_1_2',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]);
 		viajero.animations.add('turn_2_3',[21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41]);
 		viajero.animations.add('turn_3_4',[41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61]);
@@ -99,7 +118,13 @@ var GameState = {
 		this.action_right.inputEnabled = true;
 		this.action_right.events.onInputDown.add(this.moveChar,this);
 		
-	     linea = new Phaser.Line(this.game.world.centerX, 0, this.game.world.centerX, 360);
+		var ui_items = game.add.group();
+		ui_items.add(this.arrow_left);
+		ui_items.add(this.arrow_right);
+		ui_items.add(this.action_left);
+		ui_items.add(this.action_right);
+		ui_items.fixedToCamera = true;
+	     //linea = new Phaser.Line(this.game.world.centerX, 0, this.game.world.centerX, 360);
 
 		game.camera.scale = .4;
 
@@ -109,32 +134,39 @@ var GameState = {
 			this.turnChar(this.action_left);
 			
 		}
+		game.debug.spriteInfo(viajero, 10, 20);
+		//game.debug.cameraInfo(game.camera, 10, 120);
+
 	},
 	moveChar: function(sprite,event ) {
 		switch (viajero.customParams.direction){
 			case 1: 
-				var posx =  viajero.x - 36; 
-				var posy = viajero.y + 18; 
-				game.add.tween(viajero).to( { y: posy, x : posx}, 600, "Linear", true);
-    			viajero.animations.play('walk_1', 24, false);
+				var posx =  viajero.x - 19; 
+				var posy = viajero.y + 9.5; 
+				game.add.tween(viajero).to( { y: posy, x : posx}, animTime, "Linear", true);
+    			viajero.animations.play('walk_1', fps, false);
+    			viajero.animations.currentAnim.onComplete.add(function () {	viajero.frame=1}, this);
 				break;
 			case 2: 
-				var posx =  viajero.x + 36; 
-				var posy = viajero.y + 18; 
-				game.add.tween(viajero).to( { y: posy, x : posx}, 500, "Linear", true);
-    			viajero.animations.play('walk_2', 24, false);
+				var posx =  viajero.x + 19; 
+				var posy = viajero.y + 9.5; 
+				game.add.tween(viajero).to( { y: posy, x : posx}, animTime, "Linear", true);
+    			viajero.animations.play('walk_2', fps, false);
+    			viajero.animations.currentAnim.onComplete.add(function () {	viajero.frame=21}, this);
 				break;
 			case 3: 
-				var posx =  viajero.x + 36; 
-				var posy = viajero.y - 18; 
-				game.add.tween(viajero).to( { y: posy, x : posx}, 500, "Linear", true);
-    			viajero.animations.play('walk_3', 24, false);
+				var posx =  viajero.x + 19; 
+				var posy = viajero.y - 9.5; 
+				game.add.tween(viajero).to( { y: posy, x : posx}, animTime, "Linear", true);
+    			viajero.animations.play('walk_3', fps, false);
+    			viajero.animations.currentAnim.onComplete.add(function () {	viajero.frame=41}, this);
 				break;
 			case 4: 
-				var posx =  viajero.x - 36; 
-				var posy = viajero.y - 18; 
-				game.add.tween(viajero).to( { y: posy, x : posx}, 500, "Linear", true);
-    			viajero.animations.play('walk_4', 24, false);
+				var posx =  viajero.x - 19; 
+				var posy = viajero.y - 9.5; 
+				game.add.tween(viajero).to( { y: posy, x : posx}, animTime, "Linear", true);
+    			viajero.animations.play('walk_4', fps, false);
+    			viajero.animations.currentAnim.onComplete.add(function () {	viajero.frame=61}, this);
 				break;
 		}
 	},
@@ -143,16 +175,16 @@ var GameState = {
 		console.log(sprite.customParams.direction);
 		if (sprite.customParams.direction<0) {
 			if (viajero.customParams.direction ==1) {
-    			viajero.animations.play('turn_1_2', 24, false);
+    			viajero.animations.play('turn_1_2', fps, false);
 			} 
 			if (viajero.customParams.direction == 2) {
-    			viajero.animations.play('turn_2_3', 24, false);
+    			viajero.animations.play('turn_2_3', fps, false);
 			}
 			if (viajero.customParams.direction == 3) {
-    			viajero.animations.play('turn_3_4', 24, false);
+    			viajero.animations.play('turn_3_4', fps, false);
 			}
 			if (viajero.customParams.direction == 4) {
-    			viajero.animations.play('turn_4_1', 24, false);
+    			viajero.animations.play('turn_4_1', fps, false);
 			}
 			viajero.customParams.direction++;
 			if (viajero.customParams.direction==5) {
